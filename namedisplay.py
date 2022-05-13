b7f4cccf1092c538f576237d3a20ef1c1a5a2b54
DineshkumarreddyKasireddygari <105006450+DineshkumarreddyKasireddygari@users.noreply.github.com>
2022-05-13 16:14:17 +0530

    Add files via upload

---
+++ namedisplay.py
@@ -0,0 +1,9 @@
+class nameinstance:
+	def __init__(self,name):
+		self.name=name
+	#name assignment through constructor
+	def display_name(self):
+		print("Hello,My name is:",self.name)
+	#display the name
+object=nameinstance("KasireddygariDineshkumarreddy")
+object.display_name()
